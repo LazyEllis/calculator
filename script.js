0068220c058a48848b78efc7cd6b9be5dab9be0a
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2023-08-13 23:07:49 +0100

    Restructure code and fix bugs
    - Fix bug causing the screen to overflow when computing long decimals.
    - Reorder the placement of functions.
    - Add comments to the code.

--- script.js
+++ script.js
@@ -44,20 +44,23 @@ const inputNumber = (e) => {
     inputScreen.classList.contains('result')
   ) {
     if (inputScreen.classList.contains('temporary')) {
+      // The class must be removed so that the next digit doesn't overwrite the previous one.
+      // Refer to comment on line 83 for the reason
       inputScreen.classList.remove('temporary');
     } else if (inputScreen.classList.contains('result')) {
+      // The output screen must be cleared so that it doesn't interfere with your next calculation
       outputScreen.textContent = '';
+      // The class must be removed so that the next digit doesn't overwrite the previous one.
+      // Refer to comment on line 129 for the reason
       inputScreen.classList.remove('result');
     }
+    // Replaces the original zero on the calculator so you can start entering your input
     inputScreen.textContent = e.target.textContent;
-  } else if (inputScreen.textContent.length < 14) {
+  } else if (inputScreen.textContent.length < 13) {
+    // Appends the inputted digit to the end of the number
     inputScreen.textContent = `${inputScreen.textContent}${e.target.textContent}`;
   }
 };
-
-numbers.forEach((number) => {
-  number.addEventListener('click', inputNumber);
-});
 
 const chooseOperator = (e) => {
   if (
@@ -67,33 +70,29 @@ const chooseOperator = (e) => {
   ) {
     if (inputScreen.classList.contains('temporary')) {
       number = outputScreen.textContent.slice(0, -2);
+      // Replace original operator with the new inputted operator
       outputScreen.textContent = `${number} ${e.target.textContent}`;
       inputScreen.classList.remove('temporary');
     } else if (inputScreen.classList.contains('result')) {
+      // Sends the inputted number and operation to the output screen so you can enter the second number
       outputScreen.textContent = `${inputScreen.textContent} ${e.target.textContent}`;
       inputScreen.classList.remove('result');
     } else {
       outputScreen.textContent = `${inputScreen.textContent} ${e.target.textContent}`;
     }
-
+    // The temporary class makes the displayed text overwritten by your next digit input
     inputScreen.classList.add('temporary');
   } else {
-    firstNumber = outputScreen.textContent.slice(0, -2);
-    operation = outputScreen.textContent.slice(-1);
+    firstNumber = outputScreen.textContent.slice(0, -2); // Obtains number from the output screen
+    operation = outputScreen.textContent.slice(-1); // Obtains operator from the output screen
     secondNumber = inputScreen.textContent;
-    outputScreen.textContent = `${operate(
-      +firstNumber,
-      +secondNumber,
-      operation
-    )} ${e.target.textContent}`;
+    outputScreen.textContent = `${
+      operate(+firstNumber, +secondNumber, operation).toPrecision(13) / 1
+    } ${e.target.textContent}`; // Sends result and new operator when performing consecutive operations
     inputScreen.textContent = outputScreen.textContent.slice(0, -2);
     inputScreen.classList.add('temporary');
   }
 };
-
-basicOperators.forEach((operator) => {
-  operator.addEventListener('click', chooseOperator);
-});
 
 const clearEntry = () => {
   if (inputScreen.classList.contains('result')) {
@@ -102,14 +101,10 @@ const clearEntry = () => {
   inputScreen.textContent = '0';
 };
 
-clearEntryButton.addEventListener('click', clearEntry);
-
 const clear = () => {
   inputScreen.textContent = '0';
   outputScreen.textContent = '';
 };
-
-clearButton.addEventListener('click', clear);
 
 const showResult = (e) => {
   if (!inputScreen.classList.contains('result')) {
@@ -118,21 +113,32 @@ const showResult = (e) => {
     }
 
     if (outputScreen.textContent === '') {
+      // This prevents errors if they don;t input an operation i.e displays that 5 = 5
       outputScreen.textContent = `${inputScreen.textContent} ${e.target.textContent}`;
     } else {
       firstNumber = outputScreen.textContent.slice(0, -2);
       operation = outputScreen.textContent.slice(-1);
       secondNumber = inputScreen.textContent;
+      // Displays "<firstNumber> <operation> <secondNumber> = " in the output screen
       outputScreen.textContent = `${firstNumber} ${operation} ${secondNumber} ${e.target.textContent}`;
-      inputScreen.textContent = `${operate(
-        +firstNumber,
-        +secondNumber,
-        operation
-      )}`;
+      // Displays result rounded to the 13 digits
+      inputScreen.textContent = `${
+        operate(+firstNumber, +secondNumber, operation).toPrecision(13) / 1
+      }`;
     }
-
+    // The result class makes your next digit input clear your screen and overwrite the result of the displayed calculation
     inputScreen.classList.add('result');
   }
 };
 
+numbers.forEach((number) => {
+  number.addEventListener('click', inputNumber);
+});
+
+basicOperators.forEach((operator) => {
+  operator.addEventListener('click', chooseOperator);
+});
+
+clearEntryButton.addEventListener('click', clearEntry);
+clearButton.addEventListener('click', clear);
 equalButton.addEventListener('click', showResult);
